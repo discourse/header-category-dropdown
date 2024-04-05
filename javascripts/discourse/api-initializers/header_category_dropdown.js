3578aab60d7ccf94de04023b6c4c0a75a42f58bb
awesomerobot <kris.aubuchon@discourse.org>
2024-04-05 15:57:30 -0400

    DEV: update component to be compatible with glimmer header

--- javascripts/discourse/api-initializers/header_category_dropdown.js
+++ javascripts/discourse/api-initializers/header_category_dropdown.js
@@ -1,22 +1,6 @@
-import { hbs } from "ember-cli-htmlbars";
 import { apiInitializer } from "discourse/lib/api";
-import RenderGlimmer from "discourse/widgets/render-glimmer";
-import { createWidget } from "discourse/widgets/widget";
+import CustomHeaderLinks from "../components/custom-header-links";
 
-export default apiInitializer("0.11.1", (api) => {
-  createWidget("custom-header-links", {
-    html() {
-      return [
-        new RenderGlimmer(
-          this,
-          "nav.custom-header-links",
-          hbs`<CustomHeaderLinks />`
-        ),
-      ];
-    },
-  });
-
-  api.decorateWidget("home-logo:after", (helper) => {
-    return helper.attach("custom-header-links");
-  });
+export default apiInitializer("1.15.0", (api) => {
+  api.renderInOutlet("before-header-panel", CustomHeaderLinks);
 });
